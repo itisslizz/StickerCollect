6ac33f49c685c3a29cfcf313bcaec1f943a5def2
Marc Hueppin <mhueppin@gmail.com>
2021-03-19 18:22:24 +0100

    add console code

--- Panini.Console/Program.cs
+++ Panini.Console/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Panini.Console
 {
+    using Panini.Core;
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello You, Let's fill an album");
+            System.Console.WriteLine("How many stickers in a pack?");
+            var packSize = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("How many different stickers exist?");
+            var numberOfStickers = int.Parse(System.Console.ReadLine());
+            var shop = new Shop(numberOfStickers, packSize);
+
+            var user = new User(shop);
+            while(user.Album.Stickers.Count < numberOfStickers)
+            {
+                user.BuyPack();
+                user.OpenPack();
+                System.Console.WriteLine($"Packs Bought: {user.PacksBought}");
+                System.Console.WriteLine($"Stickers in Album: {user.Album.Stickers.Count}");
+                System.Console.WriteLine($"======================================");
+                
+            }
+            System.Console.WriteLine($"You had to buy {user.PacksBought} packs");
+
         }
     }
 }
