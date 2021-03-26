9564fe28546ca5f81ed2e93e81a0f7e5dbd19d77
Marc Hueppin <mhueppin@gmail.com>
2021-03-26 14:15:30 +0100

    added backend webapi

---
+++ StickerCollector.WebApi/Controllers/StickersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StickerCollector.Core;
+
+
+namespace StickerCollector.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StickersController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetSimulationData(int numberOfStickers, 
+        int packSize)
+        {
+            var dataPoints = new List<List<int>>();
+            var shop = new Shop(numberOfStickers, packSize);
+            var user = new User(shop);
+
+            while(user.Album.Stickers.Count < numberOfStickers)
+            {
+                user.BuyPack();
+                user.OpenPack();
+                dataPoints.Add(new List<int>{user.PacksBought, user.Album.Stickers.Count});
+            }
+
+            return Ok(dataPoints);
+        }
+
+    }
+}
